02d541f815ff2de93d24a551b3477dda79d37274
tinku candy <tinkucandy25@gmail.com>
2026-04-26 23:35:39 +0530

    hooks practied

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import FetchUser from "./components/useFetch";
+function App() {
+  return (
+    <>
+      <div>
+        <FetchUser userId={1} />
+      </div>
+    </>
+  );
+}
+
+export default App;
